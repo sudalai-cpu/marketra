eab07d642201ce54c4dd7fb647b98cb785473aa1
sudalai-cpu <sudalaiyandi781@gmail.com>
2026-02-07 13:38:03 +0530

    Admin CSS fix and gitignore update

--- myapp/settings.py
+++ myapp/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # Media files (Uploaded by users)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
